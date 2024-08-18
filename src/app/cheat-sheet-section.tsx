ec4389f01cec40b38b87db399cd80f4a8d67b066
Muralha <alexmuralhafernandes@gmail.com>
2024-08-18 13:18:11 -0300

    add correct types for cheat-sheet-section.tsx

--- src/app/cheat-sheet-section.tsx
+++ src/app/cheat-sheet-section.tsx
@@ -1,23 +1,25 @@
+import SectionContent from '@/app/models/section.model';
 
 interface CheatSheetSectionProps {
-  sectionContent: any;
+  sectionContent: SectionContent[];
 }
 
 const CheatSheetSection = ({ sectionContent }:CheatSheetSectionProps) => {
   return (
-    <section className="mb-8">
-      {sectionContent.map((section:any, index:number) => (
-        <div key={index} className="bg-gray-200 p-4 mb-8">
+    <div className="flex gap-3 mb-8">
+      {sectionContent.map(( section:SectionContent, idx ) => (
+        <section key={idx} className="basis-1/2 rounded-lg bg-gray-200 p-4 mb-8">
           <h2 className="text-2xl font-bold mb-4">{section.title}</h2>
           <div>
-            {section.subItems.map((subItem:any, index:number) => (
-              <div key={index} className="mb-4">
+            {section.subItems.map((subItem, idx) => (
+              <div key={idx} className="mb-4">
                 <h3 className="text-lg font-bold mb-2">{subItem.subItemTitle}</h3>
                 <div>
-                  {subItem.data.map((data:any, index:number) => (
-                    <div key={index}>
+                  {subItem.data.map((data, idx) => (
+                    <div key={idx}>
                       <p className="font-mono text-sm">
-                        <span className="text-green-800">{data.class}</span> - <span className="text-gray-500">{data.description}</span>
+                        <span className="text-neutral-800">{data.class}</span> |
+                        <span className="text-gray-500"> {data.description}</span>
                       </p>
                     </div>
                   ))}
@@ -26,9 +28,9 @@ const CheatSheetSection = ({ sectionContent }:CheatSheetSectionProps) => {
             ))
             }
           </div>
-        </div>
+        </section>
       ))}
-    </section>
+    </div>
   );
 }
 
