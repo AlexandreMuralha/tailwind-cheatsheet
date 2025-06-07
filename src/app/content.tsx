4c649df01eab5ba278f2d3afa65a4b8807abb4f1
Muralha <alexmuralhafernandes@gmail.com>
2025-06-07 08:23:15 -0300

    Add grid and gap/spacing to content

--- src/app/content.tsx
+++ src/app/content.tsx
@@ -241,4 +241,319 @@ const typography = {
   ]
 };
 
-export const content = [typography,flexbox];
+const grid = {
+  title: "Grid",
+  subItems: [
+    {
+      subItemTitle: "Grid Container",
+      data: [
+        {
+          class: 'grid',
+          description: 'sets the container as a grid container'
+        },
+        {
+          class: 'inline-grid',
+          description: 'sets the container as an inline grid container'
+        }
+      ]
+    }
+  ]
+};
+
+const gapAndSpacing = {
+  title: "Gap and Spacing",
+  subItems: [
+    {
+      subItemTitle: "Gap",
+      data: [
+        {
+          class: 'gap-0',
+          description: '0rem (0px)'
+        },
+        {
+          class: 'gap-px',
+          description: '1px'
+        },
+        {
+          class: 'gap-0.5',
+          description: '0.125rem (2px)'
+        },
+        {
+          class: 'gap-1',
+          description: '0.25rem (4px)'
+        },
+        {
+          class: 'gap-1.5',
+          description: '0.375rem (6px)'
+        },
+        {
+          class: 'gap-2',
+          description: '0.5rem (8px)'
+        },
+        {
+          class: 'gap-2.5',
+          description: '0.625rem (10px)'
+        },
+        {
+          class: 'gap-3',
+          description: '0.75rem (12px)'
+        },
+        {
+          class: 'gap-3.5',
+          description: '0.875rem (14px)'
+        },
+        {
+          class: 'gap-4',
+          description: '1rem (16px)'
+        },
+        {
+          class: 'gap-5',
+          description: '1.25rem (20px)'
+        },
+        {
+          class: 'gap-6',
+          description: '1.5rem (24px)'
+        },
+        {
+          class: 'gap-7',
+          description: '1.75rem (28px)'
+        },
+        {
+          class: 'gap-8',
+          description: '2rem (32px)'
+        },
+        {
+          class: 'gap-9',
+          description: '2.25rem (36px)'
+        },
+        {
+          class: 'gap-10',
+          description: '2.5rem (40px)'
+        },
+        {
+          class: 'gap-11',
+          description: '2.75rem (44px)'
+        },
+        {
+          class: 'gap-12',
+          description: '3rem (48px)'
+        },
+        {
+          class: 'gap-14',
+          description: '3.5rem (56px)'
+        },
+        {
+          class: 'gap-16',
+          description: '4rem (64px)'
+        },
+        {
+          class: 'gap-20',
+          description: '5rem (80px)'
+        },
+        {
+          class: 'gap-24',
+          description: '6rem (96px)'
+        },
+        {
+          class: 'gap-28',
+          description: '7rem (112px)'
+        },
+        {
+          class: 'gap-32',
+          description: '8rem (128px)'
+        },
+        {
+          class: 'gap-36',
+          description: '9rem (144px)'
+        },
+        {
+          class: 'gap-40',
+          description: '10rem (160px)'
+        },
+        {
+          class: 'gap-44',
+          description: '11rem (176px)'
+        },
+        {
+          class: 'gap-48',
+          description: '12rem (192px)'
+        },
+        {
+          class: 'gap-52',
+          description: '13rem (208px)'
+        },
+        {
+          class: 'gap-56',
+          description: '14rem (224px)'
+        },
+        {
+          class: 'gap-60',
+          description: '15rem (240px)'
+        },
+        {
+          class: 'gap-64',
+          description: '16rem (256px)'
+        },
+        {
+          class: 'gap-72',
+          description: '18rem (288px)'
+        },
+        {
+          class: 'gap-80',
+          description: '20rem (320px)'
+        },
+        {
+          class: 'gap-96',
+          description: '24rem (384px)'
+        }
+      ]
+    },
+    {
+      subItemTitle: "Spacing",
+      data: [
+        {
+          class: 'space-x-0',
+          description: '0rem (0px)'
+        },
+        {
+          class: 'space-x-px',
+          description: '1px'
+        },
+        {
+          class: 'space-x-0.5',
+          description: '0.125rem (2px)'
+        },
+        {
+          class: 'space-x-1',
+          description: '0.25rem (4px)'
+        },
+        {
+          class: 'space-x-1.5',
+          description: '0.375rem (6px)'
+        },
+        {
+          class: 'space-x-2',
+          description: '0.5rem (8px)'
+        },
+        {
+          class: 'space-x-2.5',
+          description: '0.625rem (10px)'
+        },
+        {
+          class: 'space-x-3',
+          description: '0.75rem (12px)'
+        },
+        {
+          class: 'space-x-3.5',
+          description: '0.875rem (14px)'
+        },
+        {
+          class: 'space-x-4',
+          description: '1rem (16px)'
+        },
+        {
+          class: 'space-x-5',
+          description: '1.25rem (20px)'
+        },
+        {
+          class: 'space-x-6',
+          description: '1.5rem (24px)'
+        },
+        {
+          class: 'space-x-7',
+          description: '1.75rem (28px)'
+        },
+        {
+          class: 'space-x-8',
+          description: '2rem (32px)'
+        },
+        {
+          class: 'space-x-9',
+          description: '2.25rem (36px)'
+        },
+        {
+          class: 'space-x-10',
+          description: '2.5rem (40px)'
+        },
+        {
+          class: 'space-x-11',
+          description: '2.75rem (44px)'
+        },
+        {
+          class: 'space-x-12',
+          description: '3rem (48px)'
+        },
+        {
+          class: 'space-x-14',
+          description: '3.5rem (56px)'
+        },
+        {
+          class: 'space-x-16',
+          description: '4rem (64px)'
+        },
+        {
+          class: 'space-x-20',
+          description: '5rem (80px)'
+        },
+        {
+          class: 'space-x-24',
+          description: '6rem (96px)'
+        },
+        {
+          class: 'space-x-28',
+          description: '7rem (112px)'
+        },
+        {
+          class: 'space-x-32',
+          description: '8rem (128px)'
+        },
+        {
+          class: 'space-x-36',
+          description: '9rem (144px)'
+        },
+        {
+          class: 'space-x-40',
+          description: '10rem (160px)'
+        },
+        {
+          class: 'space-x-44',
+          description: '11rem (176px)'
+        },
+        {
+          class: 'space-x-48',
+          description: '12rem (192px)'
+        },
+        {
+          class: 'space-x-52',
+          description: '13rem (208px)'
+        },
+        {
+          class: 'space-x-56',
+          description: '14rem (224px)'
+        },
+        {
+          class: 'space-x-60',
+          description: '15rem (240px)'
+        },
+        {
+          class: 'space-x-64',
+          description: '16rem (256px)'
+        },
+        {
+          class: 'space-x-72',
+          description: '18rem (288px)'
+        },
+        {
+          class: 'space-x-80',
+          description: '20rem (320px)'
+        },
+        {
+          class: 'space-x-96',
+          description: '24rem (384px)'
+        }
+      ]
+    }
+  ]
+};
+
+export const content = [typography, flexbox, grid, gapAndSpacing];
