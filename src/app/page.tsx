69f415e1f0e49f1efa7e6205011bce031c380d20
Muralha <alexmuralhafernandes@gmail.com>
2025-06-08 07:55:50 -0300

    Update padding in main layout to enhance visual spacing on the home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { content } from '@/app/content';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-24">
+    <main className="flex min-h-screen flex-col items-center justify-between px-24 pb-10">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
           <Main sectionContent={content}/>
       </div>
