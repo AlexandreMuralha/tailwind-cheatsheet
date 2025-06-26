a60a136c4ab7c2807fe886057808795d2dd21fd5
Muralha <alexmuralhafernandes@gmail.com>
2025-06-26 08:01:32 -0300

    Add sizing section to content with detailed width and height classes, including min and max

--- src/app/content.tsx
+++ src/app/content.tsx
@@ -633,4 +633,566 @@ const gapAndSpacing = {
   ],
 };
 
-export const content = [typography, flexbox, grid, alignment, gapAndSpacing];
+const sizing = {
+  title: 'Sizing',
+  subsections: [
+    {
+      subsectionTitle: 'Width and Height',
+      tooltipInfo:
+        'Control the width and height of elements. Use w-{size} for width and h-{size} for height.',
+      data: [
+        {
+          class: 'w-0',
+          description: 'width: 0px',
+        },
+        {
+          class: 'h-px',
+          description: 'height: 1px',
+        },
+        {
+          class: 'w-0.5',
+          description: 'width: 0.125rem (2px)',
+        },
+        {
+          class: 'h-1',
+          description: 'height: 0.25rem (4px)',
+        },
+        {
+          class: 'w-1.5',
+          description: 'width: 0.375rem (6px)',
+        },
+        {
+          class: 'h-2',
+          description: 'height: 0.5rem (8px)',
+        },
+        {
+          class: 'w-2.5',
+          description: 'width: 0.625rem (10px)',
+        },
+        {
+          class: 'h-3',
+          description: 'height: 0.75rem (12px)',
+        },
+        {
+          class: 'w-3.5',
+          description: 'width: 0.875rem (14px)',
+        },
+        {
+          class: 'h-4',
+          description: 'height: 1rem (16px)',
+        },
+        {
+          class: 'w-5',
+          description: 'width: 1.25rem (20px)',
+        },
+        {
+          class: 'h-6',
+          description: 'height: 1.5rem (24px)',
+        },
+        {
+          class: 'w-7',
+          description: 'width: 1.75rem (28px)',
+        },
+        {
+          class: 'h-8',
+          description: 'height: 2rem (32px)',
+        },
+        {
+          class: 'w-9',
+          description: 'width: 2.25rem (36px)',
+        },
+        {
+          class: 'h-10',
+          description: 'height: 2.5rem (40px)',
+        },
+        {
+          class: 'w-11',
+          description: 'width: 2.75rem (44px)',
+        },
+        {
+          class: 'h-12',
+          description: 'height: 3rem (48px)',
+        },
+        {
+          class: 'w-14',
+          description: 'width: 3.5rem (56px)',
+        },
+        {
+          class: 'h-16',
+          description: 'height: 4rem (64px)',
+        },
+        {
+          class: 'w-20',
+          description: 'width: 5rem (80px)',
+        },
+        {
+          class: 'h-24',
+          description: 'height: 6rem (96px)',
+        },
+        {
+          class: 'w-28',
+          description: 'width: 7rem (112px)',
+        },
+        {
+          class: 'h-32',
+          description: 'height: 8rem (128px)',
+        },
+        {
+          class: 'w-36',
+          description: 'width: 9rem (144px)',
+        },
+        {
+          class: 'h-40',
+          description: 'height: 10rem (160px)',
+        },
+        {
+          class: 'w-44',
+          description: 'width: 11rem (176px)',
+        },
+        {
+          class: 'h-48',
+          description: 'height: 12rem (192px)',
+        },
+        {
+          class: 'w-52',
+          description: 'width: 13rem (208px)',
+        },
+        {
+          class: 'h-56',
+          description: 'height: 14rem (224px)',
+        },
+        {
+          class: 'w-60',
+          description: 'width: 15rem (240px)',
+        },
+        {
+          class: 'h-64',
+          description: 'height: 16rem (256px)',
+        },
+        {
+          class: 'w-72',
+          description: 'width: 18rem (288px)',
+        },
+        {
+          class: 'h-80',
+          description: 'height: 20rem (320px)',
+        },
+        {
+          class: 'w-96',
+          description: 'width: 24rem (384px)',
+        },
+        {
+          class: 'h-auto',
+          description: 'height: auto',
+        },
+        {
+          class: 'w-full',
+          description: 'width: 100%',
+        },
+        {
+          class: 'h-screen',
+          description: 'height: 100vh',
+        },
+        {
+          class: 'w-svw',
+          description: 'width: 100svw',
+        },
+        {
+          class: 'h-lvh',
+          description: 'height: 100lvw',
+        },
+        {
+          class: 'w-dvw',
+          description: 'width: 100dvw',
+        },
+        {
+          class: 'h-min',
+          description: 'height: min-content',
+        },
+        {
+          class: 'w-max',
+          description: 'width: max-content',
+        },
+        {
+          class: 'h-fit',
+          description: 'height: fit-content',
+        },
+      ],
+    },
+    {
+      subsectionTitle: 'Minimum width/height',
+      tooltipInfo:
+        'Control the minimum width and height of elements. Use min-w-{size} for minimum width and min-h-{size} for minimum height.',
+      data: [
+        {
+          class: 'min-w-0',
+          description: 'min-width: 0px',
+        },
+        {
+          class: 'min-h-px',
+          description: 'min-height: 1px',
+        },
+        {
+          class: 'min-w-0.5',
+          description: 'min-width: 0.125rem (2px)',
+        },
+        {
+          class: 'min-h-1',
+          description: 'min-height: 0.25rem (4px)',
+        },
+        {
+          class: 'min-w-1.5',
+          description: 'min-width: 0.375rem (6px)',
+        },
+        {
+          class: 'min-h-2',
+          description: 'min-height: 0.5rem (8px)',
+        },
+        {
+          class: 'min-w-2.5',
+          description: 'min-width: 0.625rem (10px)',
+        },
+        {
+          class: 'min-h-3',
+          description: 'min-height: 0.75rem (12px)',
+        },
+        {
+          class: 'min-w-3.5',
+          description: 'min-width: 0.875rem (14px)',
+        },
+        {
+          class: 'min-h-4',
+          description: 'min-height: 1rem (16px)',
+        },
+        {
+          class: 'min-w-5',
+          description: 'min-width: 1.25rem (20px)',
+        },
+        {
+          class: 'min-h-6',
+          description: 'min-height: 1.5rem (24px)',
+        },
+        {
+          class: 'min-w-7',
+          description: 'min-width: 1.75rem (28px)',
+        },
+        {
+          class: 'min-h-8',
+          description: 'min-height: 2rem (32px)',
+        },
+        {
+          class: 'min-w-9',
+          description: 'min-width: 2.25rem (36px)',
+        },
+        {
+          class: 'min-h-10',
+          description: 'min-height: 2.5rem (40px)',
+        },
+        {
+          class: 'min-w-11',
+          description: 'min-width: 2.75rem (44px)',
+        },
+        {
+          class: 'min-h-12',
+          description: 'min-height: 3rem (48px)',
+        },
+        {
+          class: 'min-w-14',
+          description: 'min-width: 3.5rem (56px)',
+        },
+        {
+          class: 'min-h-16',
+          description: 'min-height: 4rem (64px)',
+        },
+        {
+          class: 'min-w-20',
+          description: 'min-width: 5rem (80px)',
+        },
+        {
+          class: 'min-h-24',
+          description: 'min-height: 6rem (96px)',
+        },
+        {
+          class: 'min-w-28',
+          description: 'min-width: 7rem (112px)',
+        },
+        {
+          class: 'min-h-32',
+          description: 'min-height: 8rem (128px)',
+        },
+        {
+          class: 'min-w-36',
+          description: 'min-width: 9rem (144px)',
+        },
+        {
+          class: 'min-h-40',
+          description: 'min-height: 10rem (160px)',
+        },
+        {
+          class: 'min-w-44',
+          description: 'min-width: 11rem (176px)',
+        },
+        {
+          class: 'min-h-48',
+          description: 'min-height: 12rem (192px)',
+        },
+        {
+          class: 'min-w-52',
+          description: 'min-width: 13rem (208px)',
+        },
+        {
+          class: 'min-h-56',
+          description: 'min-height: 14rem (224px)',
+        },
+        {
+          class: 'min-w-60',
+          description: 'min-width: 15rem (240px)',
+        },
+        {
+          class: 'min-h-64',
+          description: 'min-height: 16rem (256px)',
+        },
+        {
+          class: 'min-w-72',
+          description: 'min-width: 18rem (288px)',
+        },
+        {
+          class: 'min-h-80',
+          description: 'min-height: 20rem (320px)',
+        },
+        {
+          class: 'min-w-96',
+          description: 'min-width: 24rem (384px)',
+        },
+        {
+          class: 'min-h-auto',
+          description: 'min-height: auto',
+        },
+        {
+          class: 'min-w-full',
+          description: 'min-width: 100%',
+        },
+        {
+          class: 'min-h-screen',
+          description: 'min-height: 100vh',
+        },
+        {
+          class: 'min-w-svw',
+          description: 'min-width: 100svw',
+        },
+        {
+          class: 'min-h-lvh',
+          description: 'min-height: 100lvw',
+        },
+        {
+          class: 'min-w-dvw',
+          description: 'min-width: 100dvw',
+        },
+        {
+          class: 'min-h-min',
+          description: 'min-height: min-content',
+        },
+        {
+          class: 'min-w-max',
+          description: 'min-width: max-content',
+        },
+        {
+          class: 'min-h-fit',
+          description: 'min-height: fit-content',
+        },
+      ],
+    },
+    {
+      subsectionTitle: 'Maximum width/height',
+      tooltipInfo:
+        'Control the maximum width and height of elements. Use max-w-{size} for maximum width and max-h-{size} for maximum height.',
+      data: [
+        {
+          class: 'max-w-0',
+          description: 'max-width: 0rem',
+        },
+        {
+          class: 'max-h-px',
+          description: 'max-height: 1px',
+        },
+        {
+          class: 'max-w-0.5',
+          description: 'max-width: 0.125rem (2px)',
+        },
+        {
+          class: 'max-h-1',
+          description: 'max-height: 0.25rem (4px)',
+        },
+        {
+          class: 'max-w-1.5',
+          description: 'max-width: 0.375rem (6px)',
+        },
+        {
+          class: 'max-h-2',
+          description: 'max-height: 0.5rem (8px)',
+        },
+        {
+          class: 'max-w-2.5',
+          description: 'max-width: 0.625rem (10px)',
+        },
+        {
+          class: 'max-h-3',
+          description: 'max-height: 0.75rem (12px)',
+        },
+        {
+          class: 'max-w-3.5',
+          description: 'max-width: 0.875rem (14px)',
+        },
+        {
+          class: 'max-h-4',
+          description: 'max-height: 1rem (16px)',
+        },
+        {
+          class: 'max-w-5',
+          description: 'max-width: 1.25rem (20px)',
+        },
+        {
+          class: 'max-h-6',
+          description: 'max-height: 1.5rem (24px)',
+        },
+        {
+          class: 'max-w-7',
+          description: 'max-width: 1.75rem (28px)',
+        },
+        {
+          class: 'max-h-8',
+          description: 'max-height: 2rem (32px)',
+        },
+        {
+          class: 'max-w-9',
+          description: 'max-width: 2.25rem (36px)',
+        },
+        {
+          class: 'max-h-10',
+          description: 'max-height: 2.5rem (40px)',
+        },
+        {
+          class: 'max-w-11',
+          description: 'max-width: 2.75rem (44px)',
+        },
+        {
+          class: 'max-h-12',
+          description: 'max-height: 3rem (48px)',
+        },
+        {
+          class: 'max-w-14',
+          description: 'max-width: 3.5rem (56px)',
+        },
+        {
+          class: 'max-h-16',
+          description: 'max-height: 4rem (64px)',
+        },
+        {
+          class: 'max-w-20',
+          description: 'max-width: 5rem (80px)',
+        },
+        {
+          class: 'max-h-24',
+          description: 'max-height: 6rem (96px)',
+        },
+        {
+          class: 'max-w-28',
+          description: 'max-width: 7rem (112px)',
+        },
+        {
+          class: 'max-h-32',
+          description: 'max-height: 8rem (128px)',
+        },
+        {
+          class: 'max-w-36',
+          description: 'max-width: 9rem (144px)',
+        },
+        {
+          class: 'max-h-40',
+          description: 'max-height: 10rem (160px)',
+        },
+        {
+          class: 'max-w-44',
+          description: 'max-width: 11rem (176px)',
+        },
+        {
+          class: 'max-h-48',
+          description: 'max-height: 12rem (192px)',
+        },
+        {
+          class: 'max-w-52',
+          description: 'max-width: 13rem (208px)',
+        },
+        {
+          class: 'max-h-56',
+          description: 'max-height: 14rem (224px)',
+        },
+        {
+          class: 'max-w-60',
+          description: 'max-width: 15rem (240px)',
+        },
+        {
+          class: 'max-h-64',
+          description: 'max-height: 16rem (256px)',
+        },
+        {
+          class: 'max-w-72',
+          description: 'max-width: 18rem (288px)',
+        },
+        {
+          class: 'max-h-80',
+          description: 'max-height: 20rem (320px)',
+        },
+        {
+          class: 'max-w-96',
+          description: 'max-width: 24rem (384px)',
+        },
+        {
+          class: 'max-h-auto',
+          description: 'max-height: auto',
+        },
+        {
+          class: 'max-w-full',
+          description: 'max-width: 100%',
+        },
+        {
+          class: 'max-h-screen',
+          description: 'max-height: 100vh',
+        },
+        {
+          class: 'max-w-svw',
+          description: 'max-width: 100svw',
+        },
+        {
+          class: 'max-h-lvh',
+          description: 'max-height: 100lvw',
+        },
+        {
+          class: 'max-w-dvw',
+          description: 'max-width: 100dvw',
+        },
+        {
+          class: 'max-h-min',
+          description: 'max-height: min-content',
+        },
+        {
+          class: 'max-w-max',
+          description: 'max-width: max-content',
+        },
+        {
+          class: 'max-h-fit',
+          description: 'max-height: fit-content',
+        },
+      ],
+    },
+  ],
+};
+
+export const content = [
+  typography,
+  flexbox,
+  grid,
+  alignment,
+  gapAndSpacing,
+  sizing,
+];
